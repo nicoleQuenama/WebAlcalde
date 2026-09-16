6833b6cc8a5934c739c4981d3beeefad83849170
rodriknd <rp752989@gmail.com>
2026-09-16 13:28:08 -0400

    Cambiando el nombre de sobre a about

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -2,7 +2,7 @@ import type { NavItem } from '@types/nav';
 
 export const SITE_MENU: NavItem[] = [
     { nombre: 'Inicio', ruta: '/' },
-    { nombre: 'Sobre', ruta: '/sobre' },
+    { nombre: 'Sobre', ruta: '/about' },
     { nombre: 'Gestión', ruta: '/gestion' },
     { nombre: 'Noticias', ruta: '/noticias' },
     { nombre: 'Buzón Ciudadano', ruta: '/buzon' },
